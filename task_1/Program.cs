2563461cfdeb39c274cc6144e7f4ee3ef3a26bcd
“Dmitriy” <colnce1-1@ya.ru>
2022-08-11 23:47:29 +0300

    how many numbers are greater than zero

---
+++ task_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел в одну линию (не используем Enter). Числа разделены пробелами. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0 7 8 -2 -2 -> 2
+
+// 1, -7, 567, 89, 223-> 4
+
+Console.Write("Ведите число :" );
+int[] numbers = Console.ReadLine().Split().Select(x => Convert.ToInt32(x)).ToArray();
+
+int count =0;
+
+for(int i = 0; i < numbers.Length; i++) 
+{
+   if(numbers[i] > 0)
+   {
+      count++;
+      
+   }
+}
+Console.Write('[' + string.Join(", ", numbers) + ']');
+Console.Write($"Введено чисел больше нуля: {count}");
